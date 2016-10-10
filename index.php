8b8e305013937715eb0578f803e07c89b21439c6
jiwix34 <friedblattjimmybeweb@outlook.fr>
2016-10-10 09:32:45 +0200

    modif phrase copyright

--- index.php
+++ index.php
@@ -199,7 +199,7 @@
 
  </section>
 
- <p class="copyright">Développé & créé par : <a href="http://www.jimmy-friedblatt.fr/">Jimmy FRIEDBLATT</a> &<a href="http://thibaut-figueres.fr/"> Thibaut FIGUERES</a></p>
+ <p class="copyright">Développé & propulsé par : <a href="http://www.jimmy-friedblatt.fr/">Jimmy FRIEDBLATT</a> &<a href="http://thibaut-figueres.fr/"> Thibaut FIGUERES</a></p>
 
 
 </footer>
